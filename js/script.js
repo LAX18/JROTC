34f17e69ed1991b3acbc5afa9396c392a151fa44
LAX18 <34464552+LAX18@users.noreply.github.com>
2018-10-14 19:35:28 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,54 @@
 // JROTC Firebase Interaction Script
+
+// Key Handles (allows enter key to be pressed to send)
 function writePageLoad() {
   document.getElementById("date").addEventListener("keyup", function(event) {
     if (event.key === "Enter") { // event.keyCode is deprecated
-      getData();
+      formationSend();
     }
   });
   document.getElementById("uniformtype").addEventListener("keyup", function(event) {
     if (event.key === "Enter") { // event.keyCode is deprecated
-      getData();
+      formationSend();
+    }
+  });
+  document.getElementById("alpha").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") { // event.keyCode is deprecated
+     cupSend();
+    }
+  });
+  document.getElementById("alpha").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") { // event.keyCode is deprecated
+     cupSend();
     }
   });
 }
  
+// Key handle sending functions
+
+function formationSend() {
+  var array = {
+        Date: document.getElementById("date").value,
+        Uniform: document.getElementById("uniformtype").value
+      }
+      sendData("nextformation/", array);
+  document.getElementById('date').value = '';
+  document.getElementById('uniformtype').value = '';
+}
+function cupSend() {
+  var array = {
+        Alpha: document.getElementById("alpha").value,
+        Bravo: document.getElementById("bravo").value
+      }
+      sendData("commanderscup/", array);
+      document.getElementById('alpha').value = '';
+      document.getElementById('bravo').value = '';
+}
+
+
+// global read data and place on page function
 function setData(ref, element, read1) {
-  var array = readMessage(ref, function (array) {
+  var array = readData(ref, function (array) {
     document.getElementById(element).innerHTML = array[read1]
   });
 }
@@ -24,7 +59,7 @@ function sendData(ref, data) {
   firebase.database().ref(ref).set(data);
 }
  
-function readMessage(ref, callback) {
+function readData(ref, callback) {
   var nextformation = firebase.database().ref(ref);
   nextformation.on("value", function(data) {
     var array = data.val();
